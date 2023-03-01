aefc239581e15ef0dca6692e9e0ac71037ba30c5
YuGyeom Kim <rladb@MacBook-Air.local>
2023-03-02 00:54:29 +0900

    init: 개발환경 세팅

---
+++ src/main/java/com/ordermate/OrderMateApplication.java
@@ -0,0 +1,13 @@
+package com.ordermate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ordermate/OrderMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordermate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
